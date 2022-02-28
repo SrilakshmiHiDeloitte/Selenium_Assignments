5d020969d434a37c4871571424fbddff74be0866
visrilakshmi <visrilakshmi@deloitte.com>
2022-02-28 22:44:57 +0530

    Selenium Assignments

---
+++ Locators.py
@@ -0,0 +1,40 @@
+class Locators:
+
+    # Dashboard
+    temp = 'temperature' # id
+    dasb_info = 'octicon octicon-info' # class
+    buy_moist = 'Buy moisturizers' # link text
+    buy_sunsc = 'Buy sunscreens' # link text
+
+    # Selecting Items
+    items_list = '//button[@class="btn btn-primary"]' # xpath
+    Add_btn = '//button[@class="btn btn-primary"]' # xpath
+
+
+    # Moisturisers selecting item
+    moist_item1 = 'Aloe'
+    moist_item2 = 'Almond'
+
+    # Sunscreens selecting item
+    sunscr_item1 = 'SPF-50'
+    sunscr_item2 = 'SPF-30'
+
+    # Payment
+    cart_btn = 'cart' # id
+    pay_btn = 'stripe-button-el' # class name
+    frame = 'stripe_checkout_app'  # frame - pay
+    mail = 'email' # id
+    mail_in = 'sri@gmail.com' # input
+    card = 'card_number' # id
+    card_in = '4242'  # card input
+    card_date = 'cc-exp' # id
+    card_date_in1 = '02'
+    card_date_in2 = '23'
+    card_cvv = 'cc-csc' # id
+    card_cvv_in = '201'
+    pincode = 'billing-zip' # id
+    pin_in = '533125'
+    sub_pay = 'submitButton' # id
+
+
+
